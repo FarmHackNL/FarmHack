f2b20975c60b905f1d499360a73a1a977c8f89e7
Simeon Nedkov <simeon@ndkv.nl>
2016-06-18 10:53:08 +0200

    add description

--- challengers/sterenborg/scripts/vectorize-puddles.sh
+++ challengers/sterenborg/scripts/vectorize-puddles.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# Extract the puddles from Jan Clement's watershed analyses and convert them to a vector file
+
 gdalwarp -q -cutline percelen-RD.shp -crop_to_cutline -of GTiff aandeel_natte_plekken.tif anp_clipped.tif
 
 gdal_calc.py -A anp_clipped.tif --outfile=puddles.tif --calc="A==25" --NoDataValue=0
